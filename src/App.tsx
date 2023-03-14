9c1891e1ffd38583c2f763f222d9401eb51b8c22
Maxwell Silver <maxwellsilver@Maxwells-MBP.lan>
2023-03-14 19:49:34 -0400

    Setup Order service, initialize types dir, create axios request

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { axios_base } from './app/service';
-const NODE_ENV = process.env.NODE_ENV
-console.log(process.env)
 
 const App = () => {
-  axios_base.get('/Orders').then((response: any) => console.log(response));
   return (
-    <div>{NODE_ENV}</div>
+    <div></div>
   );
 }
 
---
+++ src/types/order.type.ts
@@ -0,0 +1,7 @@
+export default interface IOrder {
+    createdByUserName: string;
+    createdDate: string;
+    customerName: string;
+    orderId: string;
+    orderType: string;
+  }
