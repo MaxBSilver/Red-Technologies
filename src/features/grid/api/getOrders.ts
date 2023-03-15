677b38e7459c7c12a9068f788e5352eaea8819a3
Maxwell Silver <maxwellsilver@Maxwells-MBP.lan>
2023-03-15 11:12:02 -0400

    set orders in state

--- src/features/grid/api/getOrders.ts
+++ src/features/grid/api/getOrders.ts
@@ -3,12 +3,13 @@ import { IOrder } from "../types/order.type";
 import { http } from "./http";
 
 export const getOrders = (): Promise<IOrder[]> => {
-  return http.get(`/Orders`).then(res => res.data);
+  return http.get(`/Orders`).then(res => res?.data);
 };
 
-export const useOrders = () => {
+export const useOrders = (setOrders: any) => {
   return useQuery <IOrder[]>({
     queryKey: ["orders"],
-    queryFn: () => getOrders()
+    queryFn: () => getOrders(),
+    onSuccess: (data) => setOrders(data)
   });
 };
--- src/features/grid/components/Grid.tsx
+++ src/features/grid/components/Grid.tsx
@@ -1,23 +1,48 @@
+import { useMemo, useState } from "react";
 import { useOrders } from "../api/getOrders";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 export const Grid = () => {
-  const ordersQuery = useOrders();
-  const { data } = ordersQuery;
-  const columns: GridColDef[] = [
-    { field: "orderId", headerName: "Order ID"},
-    { field: "orderType", headerName: "Order Type" },
-    { field: "customerName", headerName: "Customer Name" },
-    { field: "createdDate", headerName: "Created Date" },
-    { field: "createdByUserName", headerName: "Created By" },
-  ];
+  // Component State
+  const [orders, setOrders] = useState([]);
+
+  // Hook for fetching orders
+  useOrders(setOrders);
+
+  // Column declarations for Datagrid
+  const columns: GridColDef[] = useMemo(
+    () => [
+      { field: "orderId", headerName: "Order ID", flex: 1, maxWidth: 300 },
+      { field: "orderType", headerName: "Order Type", flex: 1, maxWidth: 150 },
+      {
+        field: "customerName",
+        headerName: "Customer Name",
+        flex: 1,
+        maxWidth: 150,
+      },
+      {
+        field: "createdDate",
+        headerName: "Created Date",
+        flex: 1,
+        maxWidth: 200,
+      },
+      {
+        field: "createdByUserName",
+        headerName: "Created By",
+        flex: 1,
+        maxWidth: 150,
+      },
+    ],
+    []
+  );
+
   return (
     <DataGrid
-      rows={data || []}
+      rows={orders}
       columns={columns}
       getRowId={(row) => row.orderId}
       checkboxSelection
-      autoHeight	
+      autoHeight
     />
   );
 };
