c8e25fb0fcd4e4e2c79bd40d99683752e42fd220
Maxwell Silver <maxwellsilver@Maxwells-MBP.lan>
2023-03-15 14:39:39 -0400

    helper file for OrderGrid

--- src/features/grid/components/OrderGrid.tsx
+++ src/features/grid/components/OrderGrid.tsx
@@ -2,8 +2,8 @@ import { useMemo, useState } from "react";
 import { useOrders } from "../api/getOrders";
 import { DataGrid, GridColDef, GridRowSelectionModel } from "@mui/x-data-grid";
 import { OrderToolbar } from "./Toolbar";
-import { IOrder } from "../types/order.type";
 import { deleteOrders } from "../api/deleteOrders";
+import { buildColumnDefinitions, getOrderTypeFilters } from "./helper";
 
 export const OrderGrid = () => {
   // Component State
@@ -17,49 +17,14 @@ export const OrderGrid = () => {
   // Column declarations for Datagrid
   const columns: GridColDef[] = useMemo(() => {
     // Create orderType dropdown filter options
-    const orderTypeFilters: string[] = [];
-    orders.forEach((order: IOrder) => {
-      if (!orderTypeFilters.includes(order?.orderType)) {
-        orderTypeFilters.push(order?.orderType);
-      }
-    });
-
-    return [
-      { field: "orderId", headerName: "Order ID", flex: 1, maxWidth: 300 },
-      {
-        field: "orderType",
-        headerName: "Order Type",
-        type: "singleSelect",
-        valueOptions: orderTypeFilters,
-        flex: 1,
-        maxWidth: 150,
-      },
-      {
-        field: "customerName",
-        headerName: "Customer Name",
-        flex: 1,
-        maxWidth: 150,
-      },
-      {
-        field: "createdDate",
-        headerName: "Created Date",
-        flex: 1,
-        maxWidth: 200,
-      },
-      {
-        field: "createdByUserName",
-        headerName: "Created By",
-        flex: 1,
-        maxWidth: 150,
-      },
-    ];
+    const filters = getOrderTypeFilters(orders);
+    return buildColumnDefinitions(filters);
   }, [orders]);
 
-  const handleDelete =  async () => {
+  const handleDelete = async () => {
     await deleteOrders(selectedOrders);
     getOrders.refetch();
-  }
-  
+  };
 
   return (
     <DataGrid
